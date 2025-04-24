7221c2651b6a08798604112d9e60e33794b28cf6
brumal <grey040612@gmail.com>
2025-04-24 20:54:02 +0800

    首页页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,68 +1,142 @@
 // pages/home/home.js
 Page({
-  data:{
-    activeTab: 'notice' // 默认选中“通知公告”
-  },
-
-  //切换标签
-  switchTab(e) {
-    this.setData({
-        activeTab: e.currenTarget.dataset.tab
-    });
-    if (e.currenTarget.dataset.tab === "notice") {
-        //加载通知公告数据
-    } else {
-        //加载水院要闻数据
-    }
-  },
-
-  onLoad:function(options){
-    var array = this.initData();
-    this.setData({array:array});
-  },
-   initData:function(){
-    var array = [];
-    var object1 = new Object();
-    object1.img = '../images/list/food-1.jpg';
-    object1.title='关于拟推荐参加浙江省第十九届“挑战杯”大学生课外学术科技作品竞赛参赛作品公示';
-    object1.type='来源：彭秋伟';
-    object1.liulan='20696浏览';
-    object1.pinglun='7评论';
-    array[0] = object1;
-
-    var object2 = new Object();
-    object2.img = '../images/list/food-2.jpg';
-    object2.title='困了只想喝咖啡';
-    object2.type='家庭医生在线';
-    object2.liulan='29628浏览';
-    object2.pinglun='13评论';
-    array[1] = object2;
-
-    var object3 = new Object();
-    object3.img = '../images/list/food-3.jpg';
-    object3.title='橘子吃多变小黄人';
-    object3.type='家庭医生在线';
-    object3.liulan='19585浏览';
-    object3.pinglun='6评论';
-    array[2] = object3;
-
-    var object4 = new Object();
-    object4.img = '../images/list/food-4.jpg';
-    object4.title='搜狐新闻，手机用久了';
-    object4.type='广告';
-    object4.liulan='4688浏览';
-    object4.pinglun='4评论';
-    array[3] = object4;
-
-    var object5 = new Object();
-    object5.img = '../images/list/food-5.jpg';
-    object5.title='困了只想喝咖啡';
-    object5.type='家庭医生在线';
-    object5.liulan='29628浏览';
-    object5.pinglun='13评论';
-    array[4] = object5;
-
-    return array;
-  }
+    data: {
+      activeTab: 'notice', // 默认选中"通知公告"
+      noticeList: [],      // 通知公告数据
+      newsList: [],       // 水院要闻数据
+      isLoading: false    // 加载状态
+    },
   
+    // 切换标签
+    switchTab(e) {
+      const tab = e.currentTarget.dataset.tab;
+      if (this.data.activeTab === tab) return;
+      
+      this.setData({
+        activeTab: tab,
+      });
+
+      if (tab == "notice") {
+          this.loadNoticeData();
+      } else {
+          this.loadNewsData();
+      }
+  
+    },
+  
+    onLoad: function(options) {
+      // 页面加载时默认加载通知公告数据
+      this.loadNoticeData();
+      // 预加载水院要闻数据
+      this.loadNewsData();
+    },
+  
+    // 加载通知公告数据
+    loadNoticeData: function() {
+      if (this.data.noticeList.length > 0) return;
+      
+      const noticeData = this.initNoticeData();
+      this.setData({
+        noticeList: noticeData
+      });
+    },
+  
+    // 加载水院要闻数据
+    loadNewsData: function() {
+      if (this.data.newsList.length > 0) return;
+      
+      const newsData = this.initNewsData();
+      this.setData({
+        newsList: newsData
+      });
+    },
+  
+    // 初始化通知公告数据
+    initNoticeData: function() {
+      return [
+        {
+            id: 1,
+            title: '关于拟推荐参加浙江省第十九届"挑战杯"大学生课外学术科技作品竞赛参赛作品公示',
+            source: '彭秋伟',
+            viewcount: '20696浏览',
+            date: '2023-04-15'
+        },
+        {
+            id: 2,
+            title: '水利学院2023年度优秀教师评选结果公示',
+            source: '教务处',
+            viewcount: '15632浏览',
+            date: '2023-04-10'
+        },
+        {
+            id: 3,
+            title: '关于举办2023年校园开放日的通知',
+            source: '校办公室',
+            viewcount: '18945浏览',
+            date: '2023-04-05'
+        }
+      ];
+    },
+  
+    // 初始化水院要闻数据
+    initNewsData: function() {
+      return [
+        {
+            id: 101,
+            title: '水利学院成功举办2023年水利科技创新论坛',
+            source: '学院新闻中心',
+            viewcount: '32456浏览',
+            date: '2023-04-18'
+        },
+        {
+            id: 102,
+            title: '我院学子在全国大学生水利创新设计大赛中荣获特等奖',
+            source: '学工办',
+            viewcount: '28765浏览',
+            date: '2023-04-12'
+        },
+        {
+            id: 103,
+            title: '水利学院与某水利集团签署战略合作协议',
+            source: '院办',
+            viewcount: '24532浏览',
+            date: '2023-04-08'
+        },
+        {
+            id: 104,
+            title: '水利学院开展"世界水日"系列宣传活动',
+            source: '团委',
+            viewcount: '19876浏览',
+            date: '2023-03-22'
+        }
+      ];
+    },
+    
+    // 下拉刷新
+    onPullDownRefresh: function() {
+      if (this.data.activeTab === 'notice') {
+        this.setData({ noticeList: [] });
+        this.loadNoticeData();
+      } else {
+        this.setData({ newsList: [] });
+        this.loadNewsData();
+      }
+      wx.stopPullDownRefresh();
+    },
+    // 跳转到详情页
+    navigateToDetail: function(e) {
+        const id = e.currentTarget.dataset.id;
+        wx.navigateTo({
+            url: '/pages/detail/detail?id=' + id
+        });
+        },
+
+    // 查看更多
+    viewMore: function() {
+        wx.showToast({
+        title: '加载更多数据',
+        icon: 'none'
+        });
+        // 这里可以添加加载更多数据的逻辑
+    }
 });
