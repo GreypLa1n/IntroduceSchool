49e6b8517d41d98b1900ac2b2177a9e06723a689
brumal <grey040612@gmail.com>
2025-04-25 15:35:28 +0800

    增加我的页面

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,19 +1,63 @@
-// pages/me/me.js
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
-})
+    data: {
+      avatarUrl: "pagesimages\\tab\\new-0.png",  // 初始化头像 
+      defaultAvatarUrl: "pages\\images\\tab\\new-0.png",  // 默认头像路径 
+      user: {
+        name: "林穗穗",
+        gender: "女",
+        workerId: "2022b22022",
+        department: "人工智能22-2",
+        officePhone: "123456789",
+        mobile: "987654321",
+        email: "2022b22022@zjweu.edu.cn",
+        officeAddress: "综合楼801",
+      }
+    },
+  
+    onLoad: function () {
+        const avatar = wx.getStorageSync('avatarUrl');
+        if (avatar) {
+            this.setData({
+                avatarUrl: avatar
+            });
+        }
+    },
+
+    // 选择头像
+    chooseAvatar: function() {
+        wx.chooseImage({
+            count: 1,
+            sizeType: ["original", "compressed"],
+            sourceType: ["album", "camera"],
+            success: (res) => {
+                const newAvatar = res.tempFilePaths[0]  // 获取图片路径
+
+                this.setData({
+                    avatarUrl: newAvatar
+                });
+
+                wx.setStorageSync("avatarUrl", newAvatar);
+            },
+            fail: (err) => {
+                console.error("选择头像失败", err);
+            }
+        });
+    },
+
+    // 恢复默认头像
+    restoreDefaultAvatar: function (){
+        this.setData({
+            avatarUrl: this.data.defaultAvatarUrl
+        });
+
+        wx.setStorageSync('avatarUrl', this.data.defaultAvatarUrl);
+    },
+  
+    contactUser: function () {
+      // Handle contact logic
+    },
+  
+    sendEmail: function () {
+      // Handle email sending logic
+    }
+  })
